3aa298809351274afa155e000d9f81a91a585123
suzdalenko-dev <alexey.saron@gmail.com>
2025-04-08 19:36:04 +0200

    suzdalenko

---
+++ simple-factura-back/invoice/utils/vehicle_func.py
@@ -0,0 +1,11 @@
+from invoice.models.vehicledata import Vehicledata
+
+def get_or_save_vehicle(invoice_id, company_id, customer_id, matricula, other_data):
+    try:
+        vehicle, _ = Vehicledata.objects.get_or_create(company_id=company_id, customer_id=customer_id, matricula=matricula)
+        vehicle.invoice_id = invoice_id
+        vehicle.other_data = other_data
+        vehicle.save()
+        return vehicle.id
+    except Exception as e:
+        return 0
